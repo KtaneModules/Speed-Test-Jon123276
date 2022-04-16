3b832e09cd43428351862e8861dec397ccc6aecd
Jon123276 <44504288+Jon123276@users.noreply.github.com>
2022-04-16 13:19:21 -0400

    Done

--- SpeedTest-master/Assets/SpeedTest.cs
+++ SpeedTest-master/Assets/SpeedTest.cs
@@ -1,11 +1,26 @@
 ﻿using System;
 using System.Collections;
-using System.Linq;
 using UnityEngine;
+using KModkit;
+using System.Text.RegularExpressions;
 using Rnd = UnityEngine.Random;
+
+
 
 public class SpeedTest : MonoBehaviour
 {
+    public enum ColorName
+    {
+        Red,
+        Green,
+        Blue,
+        Yellow,
+        Cyan,
+        Magenta,
+        White,
+        Black
+    }
+
     public KMBombModule Module;
     public KMBombInfo BombInfo;
     public KMAudio Audio;
@@ -77,6 +92,37 @@ public class SpeedTest : MonoBehaviour
             yield return new WaitForSeconds(0.75f);
         }
     }
+    public void justForColorLogging(int pos, int color)
+    {
+        switch (color)
+        {
+            case 0:
+                Debug.LogFormat("[Speed Test #{0}] Color {1}: {2}", _moduleId, pos + 1, ColorName.Red);
+                break;
+            case 1:
+                Debug.LogFormat("[Speed Test #{0}] Color {1}: {2}", _moduleId, pos + 1, ColorName.Green);
+                break;
+            case 2:
+                Debug.LogFormat("[Speed Test #{0}] Color {1}: {2}", _moduleId, pos + 1, ColorName.Blue);
+                break;
+            case 3:
+                Debug.LogFormat("[Speed Test #{0}] Color {1}: {2}", _moduleId, pos + 1, ColorName.Yellow);
+                break;
+            case 4:
+                Debug.LogFormat("[Speed Test #{0}] Color {1}: {2}", _moduleId, pos + 1, ColorName.Cyan);
+                break;
+            case 5:
+                Debug.LogFormat("[Speed Test #{0}] Color {1}: {2}", _moduleId, pos + 1, ColorName.Magenta);
+                break;
+            case 6:
+                Debug.LogFormat("[Speed Test #{0}] Color {1}: {2}", _moduleId, pos + 1, ColorName.White);
+                break;
+            case 7:
+                Debug.LogFormat("[Speed Test #{0}] Color {1}: {2}", _moduleId, pos + 1, ColorName.Black);
+                break;
+        }
+    }
+
     private void Start()
     {
         _moduleId = _moduleIdCounter++;
@@ -90,8 +136,9 @@ public class SpeedTest : MonoBehaviour
         {
             int randValue = Rnd.Range(0, color.Length - 1);
             Color randColor = color[randValue]; // Used to get a random color used for the amount of presses.
+            justForColorLogging(i, randValue);
             randomColors[i] = randColor;
-            Debug.LogFormat("[Speed Test #{0}] Color {1}: {2}", _moduleId, i+1, randColor);
+            
         }
         StartCoroutine(cycleColors(randomColors, tempMat));
         presses = calculatePresses(randomColors);
@@ -129,10 +176,14 @@ public class SpeedTest : MonoBehaviour
         int blue = 0;
         foreach (Color color in colors)
         {
-            if (color.r == 1) red++;
-            if (color.g == 1) green++;
-            if (color.b == 1) blue++;
+            
+            if (Math.Round(color.r) == 1) red++;
+            if (Math.Round(color.g) == 1) green++;
+            if (Math.Round(color.b) == 1) blue++;
         }
+        if (red == 0) red++;
+        if (green == 0) green++;
+        if (blue == 0) blue++;
         int rgb = red * green * blue;
         timeInS = timeToTake(rgb);
         Debug.LogFormat("[Speed Test #{0}] Number of presses: {1}", _moduleId, rgb);
@@ -147,5 +198,38 @@ public class SpeedTest : MonoBehaviour
     {
         Debug.LogFormat("[Speed Test #{0}] Time to take: {1}", _moduleId, presses/5);
         return presses / 5;
+    }
+#pragma warning disable 414
+    private readonly string TwitchHelpMessage = @"!{0} press # (to press the button a number of times. Bounded from answer to answer + 100)";
+#pragma warning restore 414
+    IEnumerator ProcessTwitchCommand(string command)
+    {
+        yield return null;
+        string[] param = command.Split(' ');
+        if (Regex.IsMatch(command, @"^\s*(press)\s*$", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant))
+        {
+            int a;
+            int.TryParse(param[1], out a);
+            if (a <= presses+100)
+            {
+                Button.OnInteract();
+                yield return new WaitForSeconds(0.01f);
+            }
+            yield return "sendtochaterror Too high or too low of a number.";
+            yield break;
+        }
+        yield return "sendtochaterror Invalid command. Use help to see the commands.";
+        yield break;
+    }
+    IEnumerator TwitchHandleForcedSolve()
+    {
+        yield return null;
+        int answer = 0;
+        while (answer < presses)
+        {
+            Button.OnInteract();
+            answer++;
+            yield return new WaitForSeconds(0.01f);
+        }
     }
 }
